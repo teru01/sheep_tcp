1b364078b8cfb87e3420f5bc649ba8a944cad512
teruya <teru0x01.sheep@gmail.com>
2019-08-27 08:55:28 +0900

    add sendpacket

--- src/socket.rs
+++ src/socket.rs
@@ -8,8 +8,11 @@ use pnet::packet::tcp::{self, MutableTcpPacket, TcpFlags};
 use rand::prelude::*;
 
 use super::tcp::TcpStatus;
+const TCP_SIZE: usize = 20;
+
 
 pub struct Socket {
+	src_addr: Ipv4Addr,
 	dst_addr: Ipv4Addr,
 	pub src_port: u16,
 	dst_port: u16,
@@ -21,13 +24,13 @@ pub struct Socket {
 pub struct SendParam {
 	una: u32, //未ACK送信
 	next: u32, //次の送信
-	window: u32,
+	window: u16,
 	iss: u32, //初期送信シーケンス番号
 }
 
 pub struct RecvParam {
 	next: u32,
-	window: u32,
+	window: u16,
 	irs: u32, //初期受信シーケンスno
 }
 
@@ -35,36 +38,34 @@ impl Socket {
 	pub fn read(&self, buffer: &mut [u8]) -> Result<(), failure::Error> {
 		// 届いたデータはソケットバッファに貯めて、この関数が呼ばれた時に読み出して返す
 		// イテレータ回して受信、自分のポート以外のものは捨てる
+		unimplemented!()
 	}
 
 	pub fn write(&self) -> Result<(), failure::Error> {
-
+		unimplemented!()
 	}
 
 	pub fn handshake(&mut self) {
 
 	}
 
-	pub fn send_tcp_packet(&self, dst_addr: Ipv4Addr, dst_port: u16, my_port: u16, flag: u16, payload: Option<Vec[u8]>) -> Result<(), failure::Error> {
+	pub fn send_tcp_packet(&self, flag: u16, payload: Option<Vec<u8>>) -> Result<(), failure::Error> {
 		let (mut ts, _) = transport::transport_channel(1024, TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)))?;
-		// let data = build_packet(dst_addr, my_addr, src_port, dst_port, flag, payload)?;
-		let mut tcp_buffer = [0u8; TCP_SIZE];
-		let mut tcp_header = MutableTcpPacket::new(&mut tcp_buffer[..]).unwrap();
-		tcp_header.set_destination(dst_port);
-		tcp_header.set_source(my_port);
-
-		// オプションを含まないので、20オクテットまでがTCPヘッダ。4オクテット単位で指定する
-		tcp_header.set_data_offset(5);
-		tcp_header.set_flags(flag);
-		let checksum = tcp::ipv4_checksum(
-			&tcp_header.to_immutable(),
-			my_ipaddr,
-			&packet_info.target_ipaddr,
-		);
-		tcp_header.set_checksum(checksum);
-
-		tcp_buffer
-		ts.send_to(MutableTcpPacket::new(&mut data), IpAddr::V4(dst_addr))?;
+		let mut tcp_buffer = vec![0u8; TCP_SIZE];
+		if let Some(payload) = payload {
+			tcp_buffer.extend_from_slice(&payload)
+		};
+		let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
+		tcp_packet.set_source(self.src_port);
+		tcp_packet.set_destination(self.dst_port);
+		tcp_packet.set_sequence(self.send_param.una); // TODO: reason
+		tcp_packet.set_acknowledgement(self.recv_param.next);
+		tcp_packet.set_data_offset(5);
+		tcp_packet.set_flags(flag);
+		tcp_packet.set_window(self.send_param.window);
+		tcp_packet.set_checksum(tcp::ipv4_checksum(&tcp_packet.to_immutable(), &self.src_addr, &self.dst_addr));
+		ts.send_to(tcp_packet, IpAddr::V4(self.dst_addr))?;
+		Ok(())
 	}
 
 	// MSSとウィンドウで分割
